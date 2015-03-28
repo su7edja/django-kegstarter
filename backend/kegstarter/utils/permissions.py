89e4a8b9d3c1d04d4467d6fbbe9742ad1701faf7
Adam Burns <adam@yourteamneedsadam.com>
2015-03-28 16:13:29 -0700

    Fix syntax and model usage bugs in permissions code.

--- backend/kegstarter/utils/permissions.py
+++ backend/kegstarter/utils/permissions.py
@@ -3,6 +3,6 @@ from rest_framework.permissions import SAFE_METHODS, BasePermission
 
 class IsOwnerOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
-        owner = getattr(obj, 'owner', default=obj.user)
+        owner = getattr(obj, 'owner', obj.user)
         return bool(request.method in SAFE_METHODS or
                     request.user == owner)
--- backend/kegstarter/votingbooth/permissions.py
+++ backend/kegstarter/votingbooth/permissions.py
@@ -3,4 +3,9 @@ from rest_framework.permissions import BasePermission
 
 class PollIsNotClosed(BasePermission):
     def has_object_permission(self, request, view, obj):
-        return not obj.closed
+        if hasattr(obj, 'closed'):
+            return not obj.closed
+        elif hasattr(obj, 'poll'):
+            return not obj.poll.closed
+        else:
+            raise AttributeError('{} is not associated with a poll.'.format(obj))
