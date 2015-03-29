f953dab57f60bb14d5f5d2f7084a776f7596d4f9
sutedja <aileen.sutedja@brightscope.com>
2015-03-29 07:37:42 -0700

    api tests wip

--- backend/tests/test_kegstarter/test_kegledger/test_ledger_api.py
+++ backend/tests/test_kegstarter/test_kegledger/test_ledger_api.py
@@ -11,26 +11,6 @@ from kegstarter.kegledger.models import LedgerEntry
 
 
 class KegLedgerAPITest(APITestCase):
-    def test_ledger_list(self):
-        LedgerFactory()
-        LedgerFactory()
-        factory = APIRequestFactory()
-        request = factory.get('kegstarter/kegledger/api/ledgers')
-        view = LedgerViewSet.as_view({'get': 'list'})
-        response = view(request)
-        response.render()
-        self.assertEqual(len(response.data), 2)
-
-    def test_ledger_detail(self):
-        LedgerFactory()
-        LedgerFactory(name='Ledger 2', owner__username='UserName')
-        factory = APIRequestFactory()
-        request = factory.get('kegstarter/kegledger/api/ledgers/2')
-        view = LedgerViewSet.as_view({'get': 'retrieve'})
-        response = view(request, pk='2')
-        response.render()
-        self.assertEqual(response.data['name'], 'Ledger 2')
-
     def test_admin_can_create_ledger(self):
         pass
 
@@ -44,15 +24,18 @@ class KegLedgerAPITest(APITestCase):
         pass
 
     def test_ledger_owner_can_add_ledger_entry(self):
-        ledger = LedgerFactory(name='Ledger', owner__username='UserName')
+        from django.contrib.auth.models import Permission
+        user = UserFactory(username='UserName')
+        permission = Permission.objects.get(codename='delete_ledgerentry')
+        user.user_permissions.add(permission)
+        ledger = LedgerFactory(name='Ledger', owner=user)
         LedgerEntryFactoryRegistered(ledger=ledger)
         entry = LedgerEntryFactoryRegistered(amount=5, ledger=ledger)
-        factory = APIRequestFactory()
 
         print "before", LedgerEntry.objects.count()
         client = APIClient()
-        user = UserFactory()
-        client.login(username=user.username, password=user.password)
+        login_success = client.login(username=user.username, password=user.password)
+        print login_success
         response = client.delete(reverse('ledgerentry-detail', args=str(entry.pk)))
         print LedgerEntry.objects.count()
         print response.data
@@ -63,6 +46,19 @@ class KegLedgerAPITest(APITestCase):
         pass
 
     def test_non_ledger_owner_cannot_add_ledger_entry(self):
+        # ledger = LedgerFactory(name='Ledger', owner__username='UserName')
+        # LedgerEntryFactoryRegistered(ledger=ledger)
+        # entry = LedgerEntryFactoryRegistered(amount=5, ledger=ledger)
+        # factory = APIRequestFactory()
+        #
+        # print "before", LedgerEntry.objects.count()
+        # client = APIClient()
+        # user = UserFactory()
+        # client.login(username=user.username, password=user.password)
+        # response = client.delete(reverse('ledgerentry-detail', args=str(entry.pk)))
+        # print LedgerEntry.objects.count()
+        # print response.data
+        # print response.status_code
         pass
 
     def test_non_ledger_owner_cannot_delete_ledger_entry(self):
